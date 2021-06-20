9bf0e373e1de37d4f83a7175c358c275825653c5
Gabriel Fagundes Oczust <gabriel.oczust@meta.com.br>
2021-06-20 18:40:17 -0300

    feat: custom select

---
+++ js/select.js
@@ -0,0 +1,46 @@
+const selects = [...document.getElementsByClassName("custom-select")];
+const options = [...document.getElementsByClassName("option")];
+
+closeAllSelects = () =>
+  selects.map((select) => select.classList.remove("open"));
+
+selects.map((select) => {
+  select.addEventListener("click", (event) => {
+    closeAllSelects();
+
+    const isOpen = event.target.className.includes("open");
+
+    if (isOpen) {
+      select.classList.remove("open");
+    } else {
+      select.classList.add("open");
+    }
+  });
+});
+
+options.map((option) => {
+  option.addEventListener("click", () => {
+    option.classList.add("selected");
+
+    [...option.parentNode.children].filter((child) => {
+      if (child !== option) {
+        child.classList.remove("selected");
+      }
+    });
+
+    const valueElement = option.parentNode.parentNode.firstElementChild;
+    const optionText = option.innerText;
+
+    valueElement.innerText = optionText;
+  });
+});
+
+window.addEventListener("click", (event) => {
+  const isSelect =
+    event.target.className.includes("custom-select") ||
+    event.path[1].className.includes("custom-select");
+
+  if (!isSelect) {
+    closeAllSelects();
+  }
+});
